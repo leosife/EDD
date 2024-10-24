b56636b3c860c820da956caffaecac9d61b69054
Leonardo Fernandes <147807012+leosife@users.noreply.github.com>
2024-10-24 16:33:01 +0200

    12

--- Proyecto 3_3/src/proyecto/Clase3_3.java
+++ Proyecto 3_3/src/proyecto/Clase3_3.java
@@ -3,7 +3,7 @@ package proyecto;
 public class Clase3_3 {
 
 	public static void main(String[] args) {
-		System.out.println("“Canela En Rama”");
+		System.out.println("“Practica 3_3 Leonardo”");
 		
 
 	}
